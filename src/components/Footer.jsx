4eafde346a7e60eef22346b2d64b7d21892d222e
Atharva-04 <atharvadhokrat@gmail.com>
2024-09-29 14:30:00 +0530

    Updated footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,8 +8,7 @@ const Footer = () => {
         <p>&copy; 2024 MemePalette. All rights reserved.</p>
         <div className="social-links">
           <a href="https://github.com/Atharva-04" target="_blank" rel="noopener noreferrer">GitHub</a>
-          <a href="https://twitter.com/yourprofile" target="_blank" rel="noopener noreferrer">Twitter</a>
-          <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          <a href="https://linkedin.com/in/atharva-dhokrat" target="_blank" rel="noopener noreferrer">LinkedIn</a>
         </div>
       </div>
     </footer>
